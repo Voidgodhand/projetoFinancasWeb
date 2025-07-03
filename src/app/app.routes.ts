abc822250a96561f76ee9e84d674581fe76a0e8b
Voidgodhand <alegria.joao@hotmail.com>
2025-07-03 13:26:14 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AutenticarUsuario } from './pages/autenticar-usuario/autenticar-usuario';
+import { CriarUsuario } from './pages/criar-usuario/criar-usuario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        /* rota de navegação da página de autenticação */
+        path: 'autenticar-usuario', component: AutenticarUsuario
+    },
+    {
+        /* rota de navegação da página de cadastro */
+        path: 'criar-usuario', component: CriarUsuario
+    },
+    {
+        /* definie a rota inicial do projeto */
+        path: '', pathMatch: 'full', redirectTo: '/autenticar-usuario'
+    }
+];
---
+++ src/app/pages/autenticar-usuario/autenticar-usuario.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-autenticar-usuario',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './autenticar-usuario.html',
+  styleUrl: './autenticar-usuario.css'
+})
+export class AutenticarUsuario {
+
+  //injeção de dependência
+  http = inject(HttpClient);
+
+  //estrutura do formulário
+  form = new FormGroup({
+    email : new FormControl(''), //campo email
+    senha : new FormControl('') //campo senha
+  });
+
+  //função para executar o evento 'submit' do formulário
+  onSubmit() {
+    //fazendo uma requisição para a API de usuários
+    this.http.post('http://localhost:8082/api/v1/usuario/autenticar', this.form.value)
+      .subscribe((response) => { //capturando o retorno da API
+        console.log(response); //imprimindo no console
+      })
+  }
+}
